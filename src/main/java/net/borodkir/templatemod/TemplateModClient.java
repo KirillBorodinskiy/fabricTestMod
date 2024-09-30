447631afd764c4e85b0eb1b6ec9ad7aa2e3bd7f8
KirillBorodinskiy <kirill.borodinskiy@gmail.com>
2024-09-30 10:42:44 +0200

    Minecraft fabric mod template

---
+++ src/main/java/net/borodkir/templatemod/TemplateModClient.java
@@ -0,0 +1,10 @@
+package net.borodkir.templatemod;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class TemplateModClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
