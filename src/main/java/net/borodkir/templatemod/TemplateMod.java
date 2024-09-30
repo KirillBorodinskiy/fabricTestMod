a0fb4200090577a67357f82578149210ce6b8a74
KirillBorodinskiy <kirill.borodinskiy@gmail.com>
2024-09-30 13:17:09 +0200

    Now ruby blocks are mineable

--- src/main/java/net/borodkir/templatemod/TemplateMod.java
+++ src/main/java/net/borodkir/templatemod/TemplateMod.java
@@ -11,8 +11,9 @@ import org.slf4j.LoggerFactory;
 public class TemplateMod implements ModInitializer {
 	public static final String MOD_ID = "template-mod";
 
-
 	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
+
+
 
 	@Override
 	public void onInitialize() {
